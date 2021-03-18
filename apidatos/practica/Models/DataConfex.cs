72e2e5d2eab20a3246279444ffab8bb4dfe43fd5
sergio asbun fernandez <a2016114282@estudiantes.upsa.edu.bo>
2021-03-17 21:40:45 -0400

    Creando el data context

---
+++ apidatos/practica/Models/DataConfex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace practica.Models
+{
+    public class DataConfex : DbContext
+    {
+        public DataConfex() : base("DefaultConnection")
+        { 
+    }
+        public System.Data.Entity.DbSet<practica.Models.asbun> asbun { get; set; }
+}
